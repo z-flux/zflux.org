9f2973754805d468e8b4b66205f527f93693730b
Manarihab <manarihab5@gmail.com>
2026-02-18 19:35:07 +0200

    final fixing actions in production

--- src/app/dashboard/(userManagement)/users/_Components/PopUpMessage.tsx
+++ src/app/dashboard/(userManagement)/users/_Components/PopUpMessage.tsx
@@ -2,25 +2,24 @@
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { QueryClient, useMutation, useQueryClient } from '@tanstack/react-query'
-import React, { useState, useTransition } from 'react'
+import React, { useState } from 'react'
 import { deleteUser } from '../_Actions/deleteUser'
 
 
 
 export default function PopUpMessage({id}:{id:number}) {
-  const [isPending, startTransition] = useTransition()
   const [open,setOpen]=useState(false)
-//   const queryClient= useQueryClient()
-//        const {mutate,isPending} = useMutation({
-//   mutationFn: deleteUser,
-// })
-//   function handleDelete({id}:{id:number}){
-//     mutate({id:id},{onSuccess:()=>{
-//       queryClient.invalidateQueries({queryKey:['users']})
-//       setOpen(false)
-//     }})
+  const queryClient= useQueryClient()
+       const {mutate,isPending} = useMutation({
+  mutationFn: deleteUser,
+})
+  function handleDelete({id}:{id:number}){
+    mutate({id:id},{onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey:['users']})
+      setOpen(false)
+    }})
     
-//   }
+  }
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
   <AlertDialogTrigger asChild>
@@ -37,12 +36,7 @@ export default function PopUpMessage({id}:{id:number}) {
     </AlertDialogHeader>
     <AlertDialogFooter>
       <AlertDialogCancel>Cancel</AlertDialogCancel>
-      <AlertDialogAction onClick={() =>
-    startTransition(async () => {
-      await deleteUser({ id })
-      setOpen(false)
-    })
-  }>{isPending?<i className='tinyLoaderColored'></i>: 'Continue'}</AlertDialogAction>
+      <AlertDialogAction onClick={()=>{handleDelete({id})}}>{isPending?<i className='tinyLoaderColored'></i>: 'Continue'}</AlertDialogAction>
     </AlertDialogFooter>
   </AlertDialogContent>
 </AlertDialog>
